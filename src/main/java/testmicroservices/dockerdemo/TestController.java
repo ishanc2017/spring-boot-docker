65d15d92fe04e341ceab2de9bca99b5607cc3d4c
ishanc2017 <ishanc2017@gmail.com>
2025-06-02 04:47:32 +0530

    added cross origin annotation for CORS issue

--- src/main/java/testmicroservices/dockerdemo/TestController.java
+++ src/main/java/testmicroservices/dockerdemo/TestController.java
@@ -1,14 +1,16 @@
 package testmicroservices.dockerdemo;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 public class TestController {
 
 	@GetMapping("/test-docker")
 	public String displayTestMessage() {
-		return "Welcome to docker-microservice demo!!";
+		return "Hello, Ishan! This is a test message from the Dockerized Spring Boot application.";
 	}
 
 }
